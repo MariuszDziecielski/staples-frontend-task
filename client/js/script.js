b78284fdc75fdba50db6a818ef6bbd05eaef331a
Mariusz Dziecielski <mariuszdziec@gmail.com>
2019-06-17 18:42:17 +0200

    Add modal with product details & search functionality

--- client/js/script.js
+++ client/js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(() => {
-  let prodsCont = $('.products-container');
+  const prodsCont = $('.products-container');
+  const searchResultElem = $('.search-results');
+  const warningTextElem = $('.warning-text');
+
   let currPage = 1;
+  let product;
 
   function showProds() {
     $.ajax({
@@ -8,15 +12,25 @@ $(document).ready(() => {
       method: 'GET',
       success: response => {
         for (let i = 0; i < response.length; i++) {
-          let product = $("<div class='product'></div>");
+          product = $("<div class='product'></div>");
+
           const img = `<img src='${response[i].images.primary.large}' alt=' '>`;
           const h2 = `<h2>${response[i].general.name}</h2>`;
-          const p = `<p class'id'>${response[i].id}</p>`;
+          const p = `<p class='id'>${response[i].id}</p>`;
           const input = `<input type='number' name='quantity' min='1' id=''>`;
           const button = `<button>Dodaj</button>`;
 
-          $(product).append(img, h2, p, input, button);
+          product.append(img, h2, p, input, button);
           prodsCont.append(product);
+
+          product.find('img').click(() => {
+            $('.modal-header img').attr('src', `${response[i].images.primary.large}`);
+            $('.modal-header h5').text(`${response[i].general.name}`);
+            $('.modal-body p.id').text(`${response[i].id}`);
+            $('.modal-body div.description').html(`${response[i].general.description}`);
+            $('.modal-body p.brand').text(`${response[i].brand.name}`);
+            $('#productDetailModal').modal();
+          })
         }
       }
     });
@@ -70,7 +84,36 @@ $(document).ready(() => {
     }
   }
 
+  function searchProducts() {
+    const query = $('.search-box input').val();
+    const numOfProdsElem = $('.search-results-box .numb-of-prods');
+
+    if (query.length >= 3) {
+      $.ajax({
+        url: `http://localhost:3005/products?q=${query}&_limit=140`,
+        method: 'GET',
+        success: response => {
+          warningTextElem.hide();
+          searchResultElem.show();
+          numOfProdsElem.text(response.length);
+          $('.search-results .query').text(query);
+        }
+      });
+
+    } else {
+      searchResultElem.hide();
+      warningTextElem.show();
+    }
+  }
+
   showProds(currPage);
   showPagination();
   udatePagination();
+
+  $('.search-box button').click(e => {
+    e.preventDefault();
+    searchProducts();
+  })
+
+  searchResultElem.add(warningTextElem).hide();
 });
